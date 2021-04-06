573daf6c1060ce6bf149a5e6e20220ed429f7e5b
ncthuan <ncthuan.it@gmail.com>
2021-04-07 04:23:20 +0700

    add edit profile form, form overlay

--- public/script.js
+++ public/script.js
@@ -7,9 +7,12 @@ const isLoggedIn = () => {
 }
 
 const logout = () => {
+  if (window.location.pathname !== "/") 
+    window.location.pathname = "/" //redirect home page
   localStorage.removeItem('token');
   localStorage.removeItem('tokenExp');
   localStorage.removeItem('user');
+  $('#login-toggle').html('Login');
 }
 
 const parseJwt = (token) => {
@@ -17,26 +20,21 @@ const parseJwt = (token) => {
 }
 
 
+/* Login & signup */
 const loginPage = $('#login-page');
 const signupPage = $('#signup-page');
-
 if (!isLoggedIn()) {
   loginPage.show();
 }
 
 $('#login-toggle').click(() => {
   logout();
-  signupPage.hide();
-  $('#login-toggle').html("Login");
   loginPage.show();
 });
 
 $('#signup-toggle').click(() => {
-  loginPage.hide();
   signupPage.show();
 });
-
-
 
 $('#login-form')
   .ajaxForm({
@@ -50,7 +48,7 @@ $('#login-form')
       localStorage.setItem("token", response.token);
       alert('logged in');
       loginPage.hide();
-      $('#login-toggle').html("Logout");
+      $('#login-toggle').html('Logout');
     },
     error: (error) => {
       alert(error.responseText);
@@ -61,7 +59,6 @@ $('#signup-form')
   .ajaxForm({
     url: 'user/signup',
     type: 'POST',
-    dataType: 'json',
     success: (response) => {
       alert("user created");
       signupPage.hide();
@@ -72,20 +69,82 @@ $('#signup-form')
   });
 
 
+/* Profile */
 $('#profile-toggle').click(() => {
   //event.preventDefault();
   $.ajax({
-    url: "user/"+localStorage.username,
-    method: "GET",
-    timeout: 0,
-    headers: {
-      "Authorization": "Bearer " + localStorage.token
-    },
+    url: 'user/'+localStorage.username,
+    method: 'GET',
+    headers: { Authorization: 'Bearer '+localStorage.token },
     success: (response) => {
       $('#main').html(response);
+      setupProfileView();
     },
     error: (error) => {
       alert(error.responseText);
     }
   });
 });
+
+const setupProfileView = () => {
+  const setupEditProfileForm = () => {
+    $('#edit-profile-form')
+      .ajaxForm({
+        url: 'user/'+localStorage.username,
+        type: 'PUT',
+        headers: { Authorization: 'Bearer '+localStorage.token },
+        beforeSubmit: (arr) => {//arr: array of multipart form
+          
+        },
+        success: (response) => {
+          $('#main').html(response);
+          $('#edit-profile-page').hide();
+        },
+        error: (error) => {
+          alert(error.responseText);
+        },
+        clearForm: true
+      });
+
+    $('#input-user-img').change((event) => {
+      let preview = document.getElementById('preview-user-img');
+      preview.src = URL.createObjectURL(event.target.files[0]);
+      preview.classList.add('user-img');
+      preview.onload = () => {URL.revokeObjectURL(preview.src)}; //free memory
+      $('#preview').show();
+    });
+  }
+    
+  $('#edit-profile-button').click(() => {
+    $.ajax({
+      url: 'snippets/profile_form.html',
+      method: 'GET',
+      success: (response) => {
+        $('#edit-profile-page').html(response).show();
+        setupEditProfileForm();
+      },
+      error: (error) => {
+        alert(error.responseText);
+      }
+    });
+  });
+
+  $('#delete-profile-button').click(() => {
+    $.ajax({
+      url: 'user/'+localStorage.username,
+      method: 'DEL',
+      headers: { Authorization: 'Bearer '+localStorage.token },
+      success: (response) => {
+        alert(response.responseText);
+      },
+      error: (error) => {
+        alert(error.responseText);
+      }
+    });
+  });
+}
+
+
+const parseEditProfileFormData = (arr) => {
+
+}
